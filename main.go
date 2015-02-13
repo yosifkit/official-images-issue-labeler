69c87aa9cdc4c1ab8066851248623a11cbf16874
Joe Ferguson <joe@infosiftr.com>
2015-02-12 16:24:12 -0800

    make labels!

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
 	"os"
+	"strings"
 )
 
 type source struct {
@@ -20,6 +21,7 @@ func main() {
 		fmt.Printf("missing github access token as avg 1\n")
 		return
 	}
+
 	var NoContext oauth2.Context = nil
 	src := source{
 		myToken: &oauth2.Token{AccessToken: os.Args[1]},
@@ -27,15 +29,38 @@ func main() {
 
 	client := oauth2.NewClient(NoContext, src)
 	ghClient := github.NewClient(client)
+	owner := "docker-library"
+	repository := "official-images"
 
-	repos, _, err := ghClient.Repositories.List("", nil)
+	pulls, _, err := ghClient.PullRequests.List(owner,repository, nil)
 	if err != nil {
 		fmt.Printf("%v", err)
 		return
 	}
 
-	for key, value := range repos {
-		fmt.Printf("%d, %v\n", key, value)
+	for _, pr := range pulls {
+		commitFiles, _, err := ghClient.PullRequests.ListFiles(owner, repository, *pr.Number, nil)
+		if err != nil {
+			fmt.Printf("%v", err)
+			continue
+		}
+
+		labels := []string{}
+		for _, commitFile := range commitFiles {
+			// fmt.Printf("%s, %s: %v\n", *pr.URL, *commitFile.Filename, commitFile)
+			// fmt.Printf("PR: %s, %s\n", *pr.URL, *commitFile.Filename)
+			if strings.HasPrefix(*commitFile.Filename, "library/") {
+				labels = append(labels, *commitFile.Filename)
+			}
+		}
+		fmt.Printf("%d: %v\n", *pr.Number, labels)
+		// add labels
+		labelObjs, _, err := ghClient.Issues.AddLabelsToIssue(owner, repository, *pr.Number, labels)
+		if err != nil {
+			fmt.Printf("%v", err)
+			continue
+		}
+		fmt.Printf("%v\n", labelObjs)
 	}
 
 	return
