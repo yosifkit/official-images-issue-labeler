256ecb38cec0291a7f13368090f0b34cf125f7d3
Joe Ferguson <joe@infosiftr.com>
2015-02-13 08:35:09 -0800

    Loop over pages

--- main.go
+++ main.go
@@ -44,35 +44,45 @@ func main() {
 func labelPullsInRepo(ghClient *github.Client, owner string, repository string, state string, filePrefix string) error {
 	options := &github.PullRequestListOptions{
 		State: state,
+		ListOptions: github.ListOptions{
+			PerPage: 100,
+		},
 	}
 
-	pulls, _, err := ghClient.PullRequests.List(owner, repository, options)
-	if err != nil {
-		return err
-	}
-
-	for _, pr := range pulls {
-		commitFiles, _, err := ghClient.PullRequests.ListFiles(owner, repository, *pr.Number, nil)
+	for {
+		pulls, resp, err := ghClient.PullRequests.List(owner, repository, options)
 		if err != nil {
-			fmt.Printf("%v", err)
-			continue
+			return err
 		}
 
-		labels := []string{}
-		for _, commitFile := range commitFiles {
-			if strings.HasPrefix(*commitFile.Filename, filePrefix) {
-				labels = append(labels, *commitFile.Filename)
+		for _, pr := range pulls {
+			commitFiles, _, err := ghClient.PullRequests.ListFiles(owner, repository, *pr.Number, nil)
+			if err != nil {
+				fmt.Printf("%v", err)
+				continue
 			}
-		}
-		fmt.Printf("%d: %v\n", *pr.Number, labels)
 
-		// add labels
-		labelObjs, _, err := ghClient.Issues.AddLabelsToIssue(owner, repository, *pr.Number, labels)
-		if err != nil {
-			fmt.Printf("%v", err)
-			continue
+			labels := []string{}
+			for _, commitFile := range commitFiles {
+				if strings.HasPrefix(*commitFile.Filename, filePrefix) {
+					labels = append(labels, *commitFile.Filename)
+				}
+			}
+			fmt.Printf("%d: %v\n", *pr.Number, labels)
+
+			// add labels
+			labelObjs, _, err := ghClient.Issues.AddLabelsToIssue(owner, repository, *pr.Number, labels)
+			if err != nil {
+				fmt.Printf("%v", err)
+				continue
+			}
+			fmt.Printf("%v\n", labelObjs)
 		}
-		fmt.Printf("%v\n", labelObjs)
+
+		if resp.NextPage == 0 {
+			break
+		}
+		options.ListOptions.Page = resp.NextPage
 	}
 
 	return nil
